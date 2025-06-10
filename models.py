d4efd32e82094fb7d68f6ac8c89e097743985b59
PatrykBlacha <pblacha@student.agh.edu.pl>
2025-06-10 16:14:14 +0200

    Zmiana struktury kodu

--- models.py
+++ models.py
@@ -10,16 +10,6 @@ class User(db.Model, UserMixin):
     markers = db.relationship('Marker', backref='user', lazy=True)
     routes = db.relationship('Route', backref='user', lazy=True)
 
-
-class Marker(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    latitude = db.Column(db.Float, nullable=False)
-    longitude = db.Column(db.Float, nullable=False)
-    description = db.Column(db.Text, nullable=True)
-
-
 class Route(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -27,11 +17,19 @@ class Route(db.Model):
     description = db.Column(db.Text, nullable=True)
     waypoints = db.Column(JSON, nullable=False)  # JSON string
 
-class AvalancheMarker(db.Model):
+class MarkerBase(db.Model):
+    __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
+
+class Marker(MarkerBase):
+    __tablename__ = 'marker'
+    name = db.Column(db.String(100), nullable=False)
+
+class AvalancheMarker(MarkerBase):
+    __tablename__ = 'avalanche_marker'
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user = db.relationship('User', backref='avalanche_markers')
